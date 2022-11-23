abc5fbf8c6de40da1c7f27bccebe3973bd838ff6
russelibale808 <russelibale808@gmail.com>
2022-11-22 20:49:37 -1000

    JSVariables

--- app.js
+++ app.js
@@ -4,7 +4,31 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "My Name";
+let lastName;
+var age = 37;
 
 // Exercise 2
+lastName = "lastName";
+//firstName = "name"; (already assigned to a constant)
+age = 30;
 
 // Exercise 3
+let language = "java script";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salesPercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+// Exercise 5
+let title = "Name of the Wind";
+let suthor = "Patrick ROthfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = false;
